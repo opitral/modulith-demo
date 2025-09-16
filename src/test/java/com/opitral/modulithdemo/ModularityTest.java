06dd3e09a53e0a3a9ea7005ca9d7339943fdff48
Volodymyr Slobodian <opitral@proton.me>
2025-09-16 20:14:00 +0300

    add modulith documentation to repo

--- src/test/java/com/opitral/modulithdemo/ModularityTest.java
+++ src/test/java/com/opitral/modulithdemo/ModularityTest.java
@@ -3,8 +3,8 @@ package com.opitral.modulithdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
-class ModularityTest {
 
+class ModularityTest {
     @Test
     void createModuleDocumentation() {
         ApplicationModules modules = ApplicationModules.of(ModulithDemoApplication.class);
