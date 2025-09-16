07963c903d6a1524c18100e20a73b5b4046a292e
Volodymyr Slobodian <opitral@proton.me>
2025-09-16 20:06:23 +0300

    change document generation method

--- src/test/java/com/opitral/modulithdemo/ModularityTest.java
+++ src/test/java/com/opitral/modulithdemo/ModularityTest.java
@@ -3,12 +3,13 @@ package com.opitral.modulithdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
-
 class ModularityTest {
+
     @Test
-    void verifyAndDocument() {
-        var modules = ApplicationModules.of(ModulithDemoApplication.class);
-        modules.verify();
-        new Documenter(modules).writeDocumentation();
+    void createModuleDocumentation() {
+        ApplicationModules modules = ApplicationModules.of(ModulithDemoApplication.class);
+        new Documenter(modules)
+                .writeDocumentation()
+                .writeIndividualModulesAsPlantUml();
     }
 }
